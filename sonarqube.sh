a1a663a3beabfa51070a4e9985c97cfd405cd57a
Sagar Dahat <65307048+Sagar-Dahat@users.noreply.github.com>
2022-05-25 13:56:31 +0530

    Add files via upload

---
+++ sonarqube.sh
@@ -0,0 +1,5 @@
+sh /opt/sonar-scanner-4.7.0.2747-linux/bin/sonar-scanner \
+  -Dsonar.projectKey=Sunfire-CICD \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=http://10.18.99.152:9000 \
+  -Dsonar.login=e120122a8f619da0a67dfe8aed7910a41c900a40
