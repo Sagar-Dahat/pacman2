39fd93c1336f78f5d2a7cfc842b6f82ea3268773
Sagar Dahat <65307048+Sagar-Dahat@users.noreply.github.com>
2022-05-26 13:04:20 +0530

    Update sonarqube.sh

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,1 +1,1 @@
-/opt/sonar-scanner-4.7.0.2747-linux/bin/sonar-scanner -Dsonar.projectKey=Multibranch-project -Dsonar.sources=. -Dsonar.host.url=http://10.18.99.152:9000  -Dsonar.login=cb027b18acc6e033fd0e19f373ce8b54edbbcaf2
+/opt/sonar-scanner-4.7.0.2747-linux/bin/sonar-scanner -Dsonar.projectKey=Multibranch-project -Dsonar.sources=. -Dsonar.host.url=http://10.18.99.152:9000  -Dsonar.login=admin -Dsonar.password=redhat
